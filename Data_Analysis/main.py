a62e8b8643407af8ad877e3d739e292c3c198b08
pokerfaceSad <pokerfacesad@126.com>
2018-12-11 13:39:03 +0800

    test account

--- Data_Analysis/main.py
+++ Data_Analysis/main.py
@@ -3,8 +3,8 @@ from Theme_Analysis.visualization import WordCloudGenerater
 import settings
 
 if __name__ == "__main__":
-    Date = input("请输入查询日期(需要大于%s):\nDate" % settings.Start_Time)
-    N = eval(input("请输入查询天数:\nN"))
-    Mode = eval(input("请输入查询模式(1代表查询时Rank)\nMode"))
+    Date = input("请输入查询日期(需要大于%s):\n" % settings.Start_Time)
+    N = eval(input("请输入查询天数:\n"))
+    Mode = eval(input("请输入查询模式(1代表查询时Rank)\n"))
     Analysis = ThemeAnalysis(Date, N, Mode)
     Output = WordCloudGenerater(Analysis.freq_dict, Analysis.date_list, Analysis.query_num)
